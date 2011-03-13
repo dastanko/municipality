cee46b8fe92480c8e80b01c237fc61229a4b066c
Dastan <dastanko089@gmail.com>
2011-03-13 16:13:36 +0600

    0. Initial Setup

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'mysql2'
+gem 'haml-rails'
+gem 'jquery-rails'
+gem 'simple_form'
+gem 'rails3-generators'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'cucumber-rails'
+  gem 'ruby-debug19', :require => 'ruby-debug'
+end
+
+group :test do
+  gem 'capybara'
+  gem 'database_cleaner'
+  gem 'factory_girl_rails'
+  gem 'email_spec'
+  gem 'timecop'
+  gem 'launchy'
+end
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Municipality::Application.config.secret_token = '4290d6f3b5b6e30eb174c0a25e967250e4eb43e2f122b2f097cb0883ffa28c758d666bcd8e8c79b79f620dc0d9c75ee8e9edb6a3168d6df9743eac0d345c28de'
